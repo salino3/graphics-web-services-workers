41032e2c5f87dfd0a66b69ca845ced3094684520
Salino3 <flaviobcn012@gmail.com>
2025-07-26 11:13:05 +0200

    create page BarsGraphic

--- src/pods/home/home.component.tsx
+++ src/pods/home/home.component.tsx
@@ -1,19 +1,18 @@
 import type React from "react";
-import styles from "./home.module.scss";
 import { Link } from "react-router-dom";
+import { routesApp } from "../../router";
+import styles from "./home.module.scss";
 
 export const HomePage: React.FC = () => {
   return (
     <div className={styles.rootHomePage}>
-      <h1 aria-describedby="h1" aria-label={"Welcome!"}>
-        Welcome to My Graphics Web! 📊
-      </h1>
+      <h1 aria-describedby="h1">Welcome to My Graphics Web! 📊</h1>
       <p id="h1" className="sr_only">
         press in the graphics are you interesting for read the data
       </p>
       <div className={`centerRow btnPrimary_01 ${styles.containerAnchors}`}>
         <Link to={""}>Pie Charts</Link>
-        <Link to={""}>Bars Charts</Link>
+        <Link to={routesApp?.bars_graphic}>Bars Charts</Link>
       </div>
     </div>
   );
--- src/router/interfaces-routes.ts
+++ src/router/interfaces-routes.ts
@@ -1,11 +1,13 @@
 interface Routes {
   root: string;
   dashboard: string;
+  bars_graphic: string;
   error404: string;
 }
 
 export const routesApp: Routes = {
   root: "/graphics-web-services-workers/",
   dashboard: "/graphics-web-services-workers/dashboard/",
+  bars_graphic: "/graphics-web-services-workers/bars_graphic/",
   error404: "/graphics-web-services-workers/*",
 };
