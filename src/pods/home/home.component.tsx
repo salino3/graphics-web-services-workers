23eba94fc98f29de3c7c51503e9047c1316a5174
Salino3 <flaviobcn012@gmail.com>
2025-07-26 10:49:38 +0200

    add some styles

--- src/pods/home/home.component.tsx
+++ src/pods/home/home.component.tsx
@@ -1,20 +1,20 @@
 import type React from "react";
-import "./home.styles.scss";
+import styles from "./home.module.scss";
+import { Link } from "react-router-dom";
 
 export const HomePage: React.FC = () => {
   return (
-    <div className="rootHomePage">
-      <h1 aria-describedby="h1" tabIndex={0} aria-label={"Welcome!"}>
+    <div className={styles.rootHomePage}>
+      <h1 aria-describedby="h1" aria-label={"Welcome!"}>
         Welcome to My Graphics Web! 📊
       </h1>
       <p id="h1" className="sr_only">
         press in the graphics are you interesting for read the data
       </p>
-      <img
-        loading="lazy"
-        src={`${import.meta.env.BASE_URL}assets/images/gato-2.jpg`}
-        alt="cat image"
-      />
+      <div className={`centerRow btnPrimary_01 ${styles.containerAnchors}`}>
+        <Link to={""}>Pie Charts</Link>
+        <Link to={""}>Bars Charts</Link>
+      </div>
     </div>
   );
 };
