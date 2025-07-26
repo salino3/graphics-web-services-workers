b9da156d7cb01c3539643847a88450d96f60d950
Salino3 <flaviobcn012@gmail.com>
2025-07-26 10:34:20 +0200

    add some wcag

--- src/pods/home/home.component.tsx
+++ src/pods/home/home.component.tsx
@@ -4,13 +4,16 @@ import "./home.styles.scss";
 export const HomePage: React.FC = () => {
   return (
     <div className="rootHomePage">
-      <h1 tabIndex={0} aria-label={"Welcome!"}>
-        Welcome!
+      <h1 aria-describedby="h1" tabIndex={0} aria-label={"Welcome!"}>
+        Welcome to My Graphics Web! 📊
       </h1>
+      <p id="h1" className="sr_only">
+        press in the graphics are you interesting for read the data
+      </p>
       <img
         loading="lazy"
         src={`${import.meta.env.BASE_URL}assets/images/gato-2.jpg`}
-        alt="Main images, personal image"
+        alt="cat image"
       />
     </div>
   );
