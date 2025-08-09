049d939977552c763c95495fed886aa34a48eeec
Salino3 <flaviobcn012@gmail.com>
2025-08-09 12:03:42 +0200

    install service worker

--- src/main.tsx
+++ src/main.tsx
@@ -11,3 +11,21 @@ createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    // Register the service worker from the root of the application.
+    // This ensures that the service worker has a scope that covers all pages.
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
