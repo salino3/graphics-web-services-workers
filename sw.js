e24d08189bdbc9b652f23aea59a8e5ab22750ba7
Salino3 <flaviobcn012@gmail.com>
2025-08-10 11:35:22 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
 
 const CACHE_NAME = 'my-cache-v1';
+const OFFLINE_URL = '/graphics-web-services-workers/offline.html';
 
 // List of files to precache automatically
 const urlsToCache = [
-  "/graphics-web-services-workers/404.html",
+  ...[
   "/graphics-web-services-workers/assets\\data-generator-Bh-7_DMJ.js",
   "/graphics-web-services-workers/assets\\data-processor-BR-WKZpl.js",
   "/graphics-web-services-workers/assets\\icons\\section_01\\F_icon.svg",
   "/graphics-web-services-workers/assets\\index-Cds55-jX.css",
   "/graphics-web-services-workers/assets\\index-VSuqAebQ.js",
   "/graphics-web-services-workers/index.html"
+],
+  OFFLINE_URL
 ];
 
 self.addEventListener('install', (event) => {
@@ -17,6 +20,7 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME)
       .then((cache) => {
         console.log('Cache opened');
+        // Add all static assets and the offline page to the cache
         return cache.addAll(urlsToCache);
       })
   );
@@ -24,14 +28,24 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
+    // Try to find the resource in the cache first
     caches.match(event.request)
-      .then((response) => {
-        // Return the cached response if found
-        if (response) {
+    .then((response) => {
+      console.log('Caches', caches);
+      // If the resource is in the cache, return it
+      if (response) {
           return response;
         }
-        // If not in cache, make a network request
-        return fetch(event.request);
+
+        // If not in cache, try to fetch it from the network
+        return fetch(event.request)
+          .catch(() => {
+            // If the network request fails (because we're offline),
+            // and the request is for a navigation, serve the offline page.
+            if (event.request.mode === 'navigate') {
+              return caches.match(OFFLINE_URL);
+            }
+          });
       })
   );
 });
