48c5eb23a9f9208c58dafd6ac8822b771c01c995
Salino3 <flaviobcn012@gmail.com>
2025-08-15 10:14:54 +0200

    add pop up onLine

--- src/layout/container-layout/container-layout.layout.tsx
+++ src/layout/container-layout/container-layout.layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./container-layout.style.scss";
 
 interface Props {
@@ -6,5 +6,41 @@ interface Props {
 }
 
 export const ContainerLayout: React.FC<Props> = ({ children }) => {
-  return <div className="rootContainerLayout">{children}</div>;
+  const [isOnline, setIsOnline] = useState<string>("You are now offline.");
+  const [visible, setVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleOnline = () => {
+      setIsOnline("You are back online!");
+      setVisible(true);
+      setTimeout(() => setVisible(false), 2500);
+    };
+
+    const handleOffline = () => {
+      setIsOnline("You are now offline.");
+      setVisible(true);
+      setTimeout(() => setVisible(false), 2500);
+    };
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  console.log("onLine", navigator.onLine);
+
+  return (
+    <div className="rootContainerLayout">
+      {isOnline && (
+        <span className={`popupOnLine ${visible ? "show" : ""}`}>
+          {isOnline}
+        </span>
+      )}
+      {children}
+    </div>
+  );
 };
