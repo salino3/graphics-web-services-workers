5f399d4b224fd4f4a5b08085896c739809aa52d4
Salino3 <flaviobcn012@gmail.com>
2025-08-30 10:22:27 +0200

    fix styles arrow redirection

--- src/common-app/btn-return-back/btn-return-back.component.tsx
+++ src/common-app/btn-return-back/btn-return-back.component.tsx
@@ -1,6 +1,20 @@
 import type React from "react";
+import { useNavigate } from "react-router-dom";
+import { ArrowIcon } from "../../icons";
+import { routesApp } from "../../router";
 import "./btn-return-back.styles.scss";
 
 export const BtnReturnBack: React.FC = () => {
-  return <div className="rootBtnReturnBack">X</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="rootBtnReturnBack">
+      <ArrowIcon
+        width={28}
+        height={28}
+        customStyles={"arrowStyleIcon_01"}
+        click={() => navigate(routesApp?.root)}
+      />
+    </div>
+  );
 };
